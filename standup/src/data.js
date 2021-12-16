0c5329afc3e19af91bab69aa2a31a2b077494d47
DorkForce <77383+DorkForce@users.noreply.github.com>
2021-12-16 14:45:46 -0600

    adding standup as a subdirectory this is to replace the current HTML file at the root.  Leaving that in place until decided later

---
+++ standup/src/data.js
@@ -0,0 +1,77 @@
+export const data = {
+  "heroes": [
+    { "area": "Engineer", "name": "DanO", "leagues": ["Support"] },
+    { "area": "Engineer", "name": "JulioT", "leagues": ["Support"] },
+    { "area": "Manager", "name": "PhilipS", "leagues": ["Support"] },
+    {
+      "area": "Design",
+      "name": "MichaelD",
+      "leagues": ["Support", "Community", "Components"]
+    },
+
+    { "area": "Manager", "name": "MarceloB", "leagues": ["Community"] },
+    { "area": "Engineer", "name": "BonnieM", "leagues": ["Community"] },
+    { "area": "Engineer", "name": "JefersonG", "leagues": ["Community"] },
+    { "area": "Engineer", "name": "LauraM", "leagues": ["Community"] },
+    { "area": "Engineer", "name": "AliceL", "leagues": ["Community"] },
+    { "area": "Engineer", "name": "StevenD", "leagues": ["Community"] },
+    { "area": "Design", "name": "LeslieL", "leagues": ["Community"] },
+    { "area": "Design", "name": "NinaV", "leagues": ["Community"] },
+    { "area": "Design", "name": "DrieliL", "leagues": ["Community"] },
+    { "area": "Design", "name": "CatyN", "leagues": ["Community"] },
+    { "area": "Design", "name": "DaianaR", "leagues": ["Community"] },
+    { "area": "Design", "name": "GabrielF", "leagues": ["Community"] },
+    { "area": "Design", "name": "AdoraH", "leagues": ["Community"] },
+    { "area": "Design", "name": "CleoS", "leagues": ["Community"] },
+
+    { "area": "Manager", "name": "LisaS", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "NathanS", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "IgorA", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "AlexandreC", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "FelipeD", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "IsaacE", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "AzharS", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "CarolynT", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "WeijiaS", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "JahaanR", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "JuanO", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "LaurenG", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "MauricioC", "leagues": ["Components"] },
+    { "area": "Engineer", "name": "NikitaK", "leagues": ["Components"] },
+    { "area": "Design", "name": "LeoR", "leagues": ["Components"] },
+    { "area": "Design", "name": "RoyV", "leagues": ["Components"] },
+    { "area": "Design", "name": "FelipeB", "leagues": ["Components"] },
+    { "area": "Design", "name": "AileenF", "leagues": ["Components"] },
+    { "area": "Design", "name": "LouB", "leagues": ["Components"] },
+    { "area": "Design", "name": "MaraR", "leagues": ["Components"] },
+    { "area": "Design", "name": "AlexandreM", "leagues": ["Components"] },
+    { "area": "Design", "name": "MirandaH", "leagues": ["Components"] },
+
+    {
+      "area": "Content",
+      "name": "AimeeA",
+      "leagues": ["Support", "Community", "Components"]
+    },
+    {
+      "area": "Content",
+      "name": "RachelW",
+      "leagues": ["Support", "Community", "Components"]
+    },
+    {
+      "area": "Content",
+      "name": "RandallW",
+      "leagues": ["Support", "Community", "Components"]
+    }
+  ],
+  "leagues": [
+    {
+      "name": "Support",
+      "detail": "Tooling, Analytics, and Management of Issues"
+    },
+    {
+      "name": "Community",
+      "detail": "Website, Communications, Training, and Evangelism"
+    },
+    { "name": "Components", "detail": "DDS 1.x & DDS 2.x" }
+  ]
+};
